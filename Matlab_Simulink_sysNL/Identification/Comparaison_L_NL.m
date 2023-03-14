9225f1d8039b39c2be970781983a43cdf9f188e0
LazerTony20 <72098802+LazerTony20@users.noreply.github.com>
2023-03-14 16:06:06 -0400

    cleaned code Revue 2

--- Matlab_Simulink_sysNL/Identification/Comparaison_L_NL.m
+++ Matlab_Simulink_sysNL/Identification/Comparaison_L_NL.m
@@ -1,5 +1,5 @@
-clear all
 close all
+clear
 clc
 
 load("donnees_prof_nl.mat")
@@ -8,8 +8,10 @@ equation_lineaire
 
 Force_lineaire = FA_L(1).*(Pz-Pzeq) + FA_L(2).*(IA-Ia_e) + FA(1);
 
+%% Affichage
+close all
 
-figure();
+figure('Name','Comparaison de la Force entre la simulation linéaire et le non linéaire');
 hold on
 plot(tsim, Force_lineaire)
 plot(tsim, FA)
@@ -17,13 +19,13 @@ title('Comparaison de la Force entre la simulation linéaire et le non linéaire
 legend('Courbe de la force pour le linéaire', 'Courbe de la force pour le non linéaire')
 grid on
 
-figure();
+figure('Name','Erreur entre le linéaire et le non-linéaire');
 hold on
 plot(tsim, Force_lineaire-FA)
 title('Erreur entre le linéaire et le non-linéaire')
 grid on
 
-figure();
+figure('Name','Courbe de la force version linéaire VS non-linéaire (Agrandi)');
 hold on
 subplot(2,1,1)
 plot(tsim, Force_lineaire)
@@ -33,6 +35,6 @@ grid on
 subplot(2,1,2)
 plot(tsim, FA)
 title('Courbe de la force version non linéaire')
-legend('Courbe de la force pour le linéaire', 'Courbe de la force pour le non linéaire')
+% legend('Courbe de la force pour le linéaire', 'Courbe de la force pour le non linéaire')
 xlim([14.5 15.5])
 grid on
