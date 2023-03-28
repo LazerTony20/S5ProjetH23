22d7fd2f285f69d9126a14bca7972bd8bec5e043
LazerTony20 <72098802+LazerTony20@users.noreply.github.com>
2023-03-28 11:50:59 -0400

    Inclinaison Plaque v1
    Fichier inclinaison de la plaque (avec affichage) NON-FONCTIONNEL. Il s'agit de la version initiale préliminaire.

--- Asservissements/Asservissement_Hauteur_Plaque.m
+++ Asservissements/Asservissement_Hauteur_Plaque.m
@@ -4,3 +4,19 @@ clc
 close all
 clear
 clc
+
+% Spécifications
+% ➢ Marge de phase : 25 degrés
+% ➢ Fréquence de traverse en gain : 185 rad/s
+% ➢ Cas 1 : erreur en régime permanent à une consigne échelon : −0.0004 m pour une consigne de 0.010 m.
+% ➢ Cas 2 : erreur en régime permanent à une consigne échelon : 0 m.
+
+MP_H_P = 25;        % En degrés
+wg_H_P = 185;       % En Rads
+% Erp_ech_H_P = 
+
+
+
+
+
+
