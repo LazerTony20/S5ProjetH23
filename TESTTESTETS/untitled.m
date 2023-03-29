a35cdcff3e8aff406b9f4810d591ad8320a0f8de
Alexis Dupont <dupa1607@usherbrooke.ca>
2023-03-28 20:20:17 -0400

    a

--- TESTTESTETS/untitled.m
+++ TESTTESTETS/untitled.m
@@ -2,9 +2,12 @@ clear all
 close all
 clc
 
-pdes_real = 0;
-pdes_imag = 0i;
-delta_phi_add = 18.5;
+wn_corr = 700;
+zeta_corr = 0.005;
+delta_phi_add = 42;
+Kp_modif = 0;
+Ka_modif = 0;
+F = 12;
 
 
 MaxP = 5;
@@ -21,7 +24,10 @@ wn_tm1090 = (1 + 1.1*zeta + 1.4*zeta*zeta^2)/tr1090;
 
 wn = max([wn_ts2 wn_tp wn_tm1090]);
 
-pdes = -zeta*wn + i*wn*(1 - zeta^2)^0.5 + (pdes_real + pdes_imag)
+wn = wn + wn_corr;
+zeta = zeta + zeta_corr
+
+pdes = -zeta*wn + i*wn*(1 - zeta^2)^0.5;
 
 num_G = [9744];
 den_G = [1 31.3 -1767 -55320];
@@ -51,11 +57,11 @@ n1 = abs(polyval(num_G, pdes));
 n2 = abs(polyval(num_Ga, pdes));
 d1 = abs(polyval(den_G, pdes));
 d2 = abs(polyval(den_Ga, pdes));
-Ka = 1/(n1*n2*n2/(d1*d2*d2));
+Ka = (1/(n1*n2*n2/(d1*d2*d2)))+Ka_modif;
 
 num_Ga = (Ka^0.5)*num_Ga;
 den_Ga = den_Ga;
-Ga = tf(num_Ga, den_Ga)
+Ga = tf(num_Ga, den_Ga);
 
 % figure();
 % hold on
@@ -65,7 +71,30 @@ Ga = tf(num_Ga, den_Ga)
 % plot(real(p_1), imag(p_1), 'square')
 
 
+Ga2G = Ga*Ga*G;
+
+% figure()
+% hold on
+% stepinfo(feedback(Ga2G,1))
+% step(feedback(Ga2G,1))
+
+% Création du PI
+
+[num_Ga2G, den_Ga2G] = tfdata(Ga2G, 'v');
+z = real(pdes)/F;
+kp = 1 + Kp_modif;
+
+num_Gpi = kp*[1 -z];
+den_Gpi = [1 0];
+Gpi = tf(num_Gpi, den_Gpi);
+
+GpiGa2G = Gpi*Ga2G;
+
 figure()
 hold on
-stepinfo(feedback(Ga*Ga*G,1))
-step(feedback(Ga*Ga*G,1))
+stepinfo(feedback(GpiGa2G,1))
+t = [0:0.0001:0.1];
+u = ones(size(t));
+lsim(feedback(GpiGa2G,1), u, t)
+[Gm,Pm,Wcg,Wcp] = margin(GpiGa2G)
+
